255a56bc8261a886caf86750b1bce0d5c7d26dad
Tom Nyone <tom.nyone@gmail.com>
2020-05-10 17:52:28 +0100

    First commit

---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+if(document.images) {
+   var image1 = new Image();     // Preload an image
+   image1.src = "https://images-na.ssl-images-amazon.com/images/I/71%2B9ANTbShL._AC_UY535_.jpg";
+   var image2 = new Image();     // Preload second image
+   image2.src = "https://images-na.ssl-images-amazon.com/images/I/71sFE5yya4L._AC_UY500_.jpg";
+}
+
+if(document.images) {
+   var image2_1 = new Image();     // Preload an image
+   image2_1.src = "https://images-na.ssl-images-amazon.com/images/I/71CzQ87dE2L._AC_UY500_.jpg";
+   var image2_2 = new Image();     // Preload second image
+   image2_2.src = "https://images-na.ssl-images-amazon.com/images/I/61g7%2BxhvnSL._AC_UY500_.jpg";
+}
+
+if(document.images) {
+   var image3_1 = new Image();     // Preload an image
+   image3_1.src = "https://images-na.ssl-images-amazon.com/images/I/61c9zMnPDeL._AC_UY675_.jpg";
+   var image3_2 = new Image();     // Preload second image
+   image3_2.src = "https://images-na.ssl-images-amazon.com/images/I/61PuIMIXf%2BL._AC_UY535_.jpg";
+}
+     
+if(document.images) {
+   var image4_1 = new Image();     // Preload an image
+   image4_1.src = "https://images-na.ssl-images-amazon.com/images/I/61c9zMnPDeL._AC_UY675_.jpg";
+   var image4_2 = new Image();     // Preload second image
+   image4_2.src = "https://images-na.ssl-images-amazon.com/images/I/61PuIMIXf%2BL._AC_UY535_.jpg";
+}
+    
